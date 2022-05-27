8f35ff724aaffef7c20c091f5cc5583ce3d9c4b8
ghostfxck <47380928+ghostfxck@users.noreply.github.com>
2022-05-27 15:42:07 +0800

    Add files via upload

---
+++ krnlautokey.bat
@@ -0,0 +1,18 @@
+@echo off
+color 0A
+set /a a=1
+:a
+cls
+start https://cdn.krnl.ca/getkey.php
+echo [%a%] Please solve the CAPTCHA, click the submit button then press any key to continue...
+pause > nul
+echo Please wait for 15 seconds
+timeout /t 15 /nobreak
+set /a a=%a%+1
+if "%a%"=="5" (
+    echo done!
+    start https://cdn.krnl.ca/getkey.php
+    pause
+    exit
+)
+goto a
