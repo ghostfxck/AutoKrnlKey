aaed893fd5fe0f497feff9e2169a5ffce839399d
ghostfxck <47380928+ghostfxck@users.noreply.github.com>
2022-08-16 20:50:01 +0800

    Update krnlautokey.bat

--- krnlautokey.bat
+++ krnlautokey.bat
@@ -3,7 +3,7 @@ color 0A
 set /a a=1
 :a
 cls
-start https://cdn.krnl.ca/getkey.php
+start https://cdn.krnl.place/getkey.php
 echo [%a%] Please solve the CAPTCHA, click the submit button then press any key to continue...
 pause > nul
 echo Please wait for 15 seconds
